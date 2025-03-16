44b0223d93b9d4d7c6cdf47826a4494d8bc98f3d
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-16 23:13:08 +0530

    Insert At Begin Added in Linkedlist

--- LinkedList/Insertatbegin/insertatbegin.java
+++ LinkedList/Insertatbegin/insertatbegin.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package LinkedList.Insertatbegin;
 
 class Node{
     int data;
@@ -9,18 +9,20 @@ class Node{
     }
 }
 
-
-public class Implementation {
+public class insertatbegin {
     public static void main(String[] args) {
         Node head = new Node(10);
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
 
+        Node temp = new Node(5);
+        temp.next = head;
+        head = temp;
 
         Node curr = head;
         while(curr != null){
-            System.out.println(curr.data + " ");
+            System.out.print(curr.data + " ");
             curr = curr.next;
         }
     }
