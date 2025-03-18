0113573ff5375b7700171c86dabe77fb920641b2
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-18 23:50:18 +0530

    Insert At End

---
+++ LinkedList/InsertatEnd/insertatEnd.java
@@ -0,0 +1,23 @@
+package LinkedList.InsertatEnd;
+
+class Node{
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public Node insertatEnnd(Node head){
+
+}
+
+public class insertatEnd {
+    public static void main(String[] args) {
+        
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+    }
+}
