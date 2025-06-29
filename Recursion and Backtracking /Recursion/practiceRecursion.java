ca23ba35d28aab9f74a80f65cc60c4d553e39838
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-06-29 23:49:28 +0530

    Recursion Practice

---
+++ Recursion and Backtracking /Recursion/practiceRecursion.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class practiceRecursion {
+    public static void main(String[] args) {
+        countSum(5);
+        System.out.println();
+    }
+
+    public static void countSum(int n){
+        if(n == 0)
+        return ;
+
+        System.out.println(n + " ");
+        countSum(n - 1);
+    }
+}
