a69d86665bd8420a3603d73f55e1aac9490d159c
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-02 23:08:57 +0530

    Array

--- Array/implementation.java
+++ Array/implementation.java
@@ -12,8 +12,9 @@ public class implementation {
                 arr[i] = sc.nextInt();
         }
 
+        System.out.println("Printing the elements");
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         } 
     }
 }
