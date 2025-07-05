dca14c4fb880364e46f77e88c74cfe025b970f0d
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-05 23:26:52 +0530

    Hashing

--- Array/MethodInJava.java
+++ Array/MethodInJava.java
@@ -3,6 +3,15 @@ package Array;
 import java.util.*;
 
 public class MethodInJava {
-    Array<Integer> arr = new Array<>();
+//    ArrayList<Integer> arr = new ArrayList<>();
+//    List<Integer> list = new ArrayList<>();
 
+//    list.add(10);
+//    list.add(20);
+//    list.add(30);
+
+    List<Integer> list  = Arrays.asList(10,20,30);
+    for(int i = 0;i<list.size();i++){
+        System.out.println();
+    }
 }
