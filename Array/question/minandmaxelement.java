5c8d6eddefb2bd5ff8982d513b75f5a23561723e
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-06 23:20:20 +0530

    Array

---
+++ Array/question/minandmaxelement.java
@@ -0,0 +1,18 @@
+package Array.question;
+
+public class minandmaxelement {
+    public static void main(String[] args) {
+        int arr[] = {1,-6};
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i<arr.length;i++){
+            min = Math.min(min,arr[i]);
+            max = Math.max(max,arr[i]);
+        }
+
+        System.out.println("Maximum Value : " + max);
+        System.out.println("Min : " + min);
+    }
+}
