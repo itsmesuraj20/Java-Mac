6c13bfc759e91595f9d9d2c4cd1ef880cdd7904c
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-07-09 00:00:07 +0530

    Javax

--- Array/question/minandmaxelement.java
+++ Array/question/minandmaxelement.java
@@ -7,6 +7,7 @@ public class minandmaxelement {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        
         for(int i = 0; i<arr.length;i++){
             min = Math.min(min,arr[i]);
             max = Math.max(max,arr[i]);
